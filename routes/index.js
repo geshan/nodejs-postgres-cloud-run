19a1a35bddb05d0ce22c4ee2f8ff7a136e5b1fd0
Geshan Manandhar <geshan@simplywallst.com>
2023-05-08 17:14:49 +1000

    To demo traffic-percent 50%

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+  res.json({message: 'alive - 50'});
 });
 
 module.exports = router;

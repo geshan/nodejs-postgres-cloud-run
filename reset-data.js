4c78ffd26f9d26bcc385f0a050a81841a2edb0d0
Geshan Manandhar <geshan@simplywallst.com>
2024-04-22 22:11:09 +1000

    Reset data

--- reset-data.js
+++ reset-data.js
@@ -1,7 +1,7 @@
 const db = require('./services/db');
 
 (async () => {
-  await db.query(`TRUNCATE quote`);
+  await db.query(`TRUNCATE quote RESTART IDENTITY`);
   await db.query(`INSERT INTO quote (id, quote, author) VALUES 
   (1, 'There are only two kinds of languages: the ones people complain about and the ones nobody uses.', 'Bjarne Stroustrup'), 
   (2, 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'Martin Fowler'), 
